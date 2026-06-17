5ac347da88ba9c8f8687b9105acae6e453372fad
colindouglas <colin.douglas@cds-snc.ca>
2026-06-17 12:18:46 -0300

    refactor: move colour definitions

--- R/connection.R
+++ R/connection.R
@@ -18,7 +18,7 @@
 
 required_packages <- c(
   "DBI", "RAthena", "dplyr", "dbplyr", "stringr", "tidyr", "lubridate",
-  "ggplot2", "scales", "dotenv"
+  "ggplot2", "scales", "cowplot", "magick", "dotenv"
 )
 
 check_packages <- function() {
